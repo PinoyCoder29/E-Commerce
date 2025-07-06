cea3b916dd372861f5ce6619f2128926b255ad40
PinoyCoder29 <bicojayvee4@gmail.com>
2025-07-06 10:13:46 +0800

    update app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import MainLayout from '../layout/MainLayout'
-import SignUp from '../pages/mainPages/SIgnUp'
+import SignUp from '../pages/mainPages/SignUp'
 
 
 const App = () => {
